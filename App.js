e4ac63cc41c49ed698ffea5b23b1a591deff8041
M. Hafiz Bukhari <hafiz.bukhari@hotmail.com>
2021-03-17 12:29:58 +0700

    First

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import {View, Text, TextInput, StyleSheet, Button, Alert, BackHandler} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  const [data, setData] = useState({
+    username:"",
+    password:""
+  })
+
+  function HandleSubmit(){
+    const {username, password} = data
+    username==="hafiz" && password ==="hafiz"?
+      Alert.alert("Judul Alert", "Success"):
+      Alert.alert("Judul Alert", "fail")
+    
+  }
+ 
+  return(
+    <>
+        <View style={myStyle.container}>
+          <View style={myStyle.card}>
+            <TextInput
+              placeholder="username"
+              style = {myStyle.inputStyle}
+              onChangeText = {e=>{
+                setData({...data,username:e})
+                console.log(data)
+              }
+             }
+            />
+            <TextInput
+              placeholder="password"
+              style = {myStyle.inputStyle}
+              onChangeText = {e=>{
+                setData({...data,password:e})
+                console.log(data)
+              }
+             }
+            />
+            <Button
+              title="Submit"
+              onPress={HandleSubmit}
+            />
+          </View>
+        </View>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const myStyle = StyleSheet.create({
+  inputStyle:{
+    padding:5,
+    fontSize:15,
+    marginVertical:45,
+    margin:"auto",
   },
-});
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"blue"
+  },
+  card:{
+    display:"flex",
+    justifyContent:"center",
+    width:250,
+    height:350,
+    backgroundColor:'white'
+  }
+})
